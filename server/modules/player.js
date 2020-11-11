2d80cc36e4e31d6864893c5d8b845969d1ad907b
Nikos Cocci <coccinikos@gmail.com>
2020-11-11 15:45:49 +0100

    added server, created modules

---
+++ server/modules/player.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+  socket: String,
+  room: String,
+  name: String,
+  votes: { type: Number, default: 0 },
+  done: { type: Boolean, default: false },
+  problem: String,
+  drawing: {
+    name: String,
+    url: String
+  }
+});
+
+module.exports = mongoose.model('Player', playerSchema);
