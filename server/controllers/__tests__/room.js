23baefb7a16f437772a1834decace84ab7e04732
P.Ackroyd <peter.r.ackroyd@gmail.com>
2020-11-23 11:28:55 +0000

    fix(server): stopped ability to join games which have been started
    Added a gameStarted bool to room schema which is updated when players leave the lobby to start a
    game and checked when players are joining a lobby

--- server/controllers/__tests__/room.js
+++ server/controllers/__tests__/room.js
@@ -1,10 +1,11 @@
 const {db} = require('../../models');
-const {getAvailableRoom, updateRoom} = require('../room');
+const {getAvailableRoom, updateRoom, checkRoomStatus, updateStartGameStatus } = require('../room');
 
 jest.mock('../../models');
 
 db.Room.aggregate.mockResolvedValue([{code: 'ASS', active: true}]);
 db.Room.updateOne.mockResolvedValue();
+db.Room.findOne.mockResolvedValue({active: true,gameStarted: false,code: 'ASS'});
 
 describe('getAvailableRoom()', () => {
   test('should get an inactive room', async () => {
@@ -34,7 +35,30 @@ describe('updateRoom()', () => {
   });
 });
 
+describe('checkRoomStatus()', () => {
+  test('should return the data for the room', async () => {
+    const res=await checkRoomStatus('ASS');
+
+    expect(db.Room.findOne).toBeCalledWith(
+      {code: 'ASS'},
+    );
+    expect(res).toEqual({active: true,gameStarted: false,code: 'ASS'});
+  });
+});
+
+describe('updateStartGameStatus()', () => {
+  test('should set the start status of the game to true', async () => {
+    const res=await updateStartGameStatus('ASS');
+
+    expect(db.Room.updateOne).toBeCalledWith(
+      {code: 'ASS'},
+      {$set: {gameStarted: true}},
+    );
+  });
+});
+
 afterAll(() => {
   db.Room.aggregate.mockReset();
   db.Room.updateOne.mockReset();
+  db.Room.findOne.mockReset();
 });
