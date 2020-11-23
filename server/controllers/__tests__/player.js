c5997d2ce5ac33ae3f640ba74da835fcaeb279a9
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-11-23 18:53:28 +0300

    style: formating

--- server/controllers/__tests__/player.js
+++ server/controllers/__tests__/player.js
@@ -10,35 +10,38 @@ const {
   upvoteDrawing,
   getLeaderboard,
   getPlayerBySocket,
-  removePlayer} = require('../player');
+  removePlayer,
+} = require('../player');
 
 jest.mock('../../models');
 
-
-const mockRoom='ASS';
-const mockProblem='I want to drive and A, at the same time';
-const mockName='John';
-const mockDrawing={
-  name:'inventionName',
-  url:'data'
-}
-const mockSocket='a1bc2DEFGhiJkL3MNOPQ';
-const mockPlayer={
-  votes:0,
-  done:false,
-  socket:mockSocket,
-  room:mockRoom,
-  name:'Jill',
-  problem:mockProblem}
-const mockPlayers=[
+const mockRoom = 'ASS';
+const mockProblem = 'I want to drive and A, at the same time';
+const mockName = 'John';
+const mockDrawing = {
+  name: 'inventionName',
+  url: 'data',
+};
+const mockSocket = 'a1bc2DEFGhiJkL3MNOPQ';
+const mockPlayer = {
+  votes: 0,
+  done: false,
+  socket: mockSocket,
+  room: mockRoom,
+  name: 'Jill',
+  problem: mockProblem,
+};
+const mockPlayers = [
   mockPlayer,
   {
-  votes:1,
-  done:true,
-  socket:mockSocket,
-  room:mockRoom,
-  name:'John',
-  problem:'I want to drive and B, at the same time'}]
+    votes: 1,
+    done: true,
+    socket: mockSocket,
+    room: mockRoom,
+    name: 'John',
+    problem: 'I want to drive and B, at the same time',
+  },
+];
 
 db.Player.find.mockResolvedValue(mockPlayers);
 db.Player.updateOne.mockResolvedValue();
@@ -55,83 +58,105 @@ db.Player.mockImplementation(() => {
 
 describe('postPlayer()', () => {
   test('should post a player', async () => {
-    const res=await postPlayer(mockPlayer);
+    const res = await postPlayer(mockPlayer);
 
     expect(mockFn).toBeCalled();
     expect(res).toBe(mockPlayer);
   });
 
   test('getPlayersInRoom() should find players in a room', async () => {
-    const res=await getPlayersInRoom(mockRoom)
+    const res = await getPlayersInRoom(mockRoom);
 
-    expect(db.Player.find).toBeCalledWith({room: mockRoom}, expect.any(Function));
+    expect(db.Player.find).toBeCalledWith(
+      {room: mockRoom},
+      expect.any(Function),
+    );
     expect(res).toBe(mockPlayers);
   });
 
   test('updateProblem() should change the problem for a specific player', async () => {
-    const res=await updateProblem(mockProblem, mockName, mockRoom)
+    const res = await updateProblem(mockProblem, mockName, mockRoom);
 
     expect(db.Player.updateOne).toBeCalledWith(
       {name: mockName, room: mockRoom},
-      {$set: {problem: mockProblem, done: true}},);
+      {$set: {problem: mockProblem, done: true}},
+    );
   });
 
   test('setDone() should update one players done status', async () => {
-    await setDone(mockName, mockRoom)
+    await setDone(mockName, mockRoom);
 
-    expect(db.Player.updateOne).toBeCalledWith({name: mockName, room: mockRoom}, {$set: {done: true}});
+    expect(db.Player.updateOne).toBeCalledWith(
+      {name: mockName, room: mockRoom},
+      {$set: {done: true}},
+    );
   });
 
   test('allDone() should return false when not all players are done', async () => {
-    const res=await allDone(mockRoom)
+    const res = await allDone(mockRoom);
 
     expect(db.Player.find).toBeCalledWith({room: mockRoom});
     expect(res).toBe(false);
   });
 
   test('resetDone() updates done for an entire room', async () => {
-    await resetDone(mockRoom)
+    await resetDone(mockRoom);
 
-    expect(db.Player.updateMany).toBeCalledWith({room: mockRoom}, {$set: {done: false}});
+    expect(db.Player.updateMany).toBeCalledWith(
+      {room: mockRoom},
+      {$set: {done: false}},
+    );
   });
 
   test('updateDrawing() sets the drawing for one player', async () => {
-    await updateDrawing(mockSocket,mockDrawing);
+    await updateDrawing(mockSocket, mockDrawing);
 
-    expect(db.Player.updateOne).toBeCalledWith({socket: mockSocket}, {$set: {drawing: mockDrawing, done: true}});
+    expect(db.Player.updateOne).toBeCalledWith(
+      {socket: mockSocket},
+      {$set: {drawing: mockDrawing, done: true}},
+    );
   });
 
   test('upvoteDrawing() increases the vote for one player', async () => {
-    await upvoteDrawing(mockName,mockRoom);
+    await upvoteDrawing(mockName, mockRoom);
 
-    expect(db.Player.updateOne).toBeCalledWith({name: mockName, room: mockRoom}, {$inc: {votes: 1}});
+    expect(db.Player.updateOne).toBeCalledWith(
+      {name: mockName, room: mockRoom},
+      {$inc: {votes: 1}},
+    );
   });
 
   test('getLeaderboard() gets players and sorts their names by score', async () => {
-    const res=await getLeaderboard(mockRoom);
+    const res = await getLeaderboard(mockRoom);
 
     expect(db.Player.find).toBeCalledWith({room: mockRoom});
-    expect(res).toEqual([{
-      name: 'Jill',
-      votes: 0},
-      {name: 'John',
-      votes: 1}]);
+    expect(res).toEqual([
+      {
+        name: 'Jill',
+        votes: 0,
+      },
+      {name: 'John', votes: 1},
+    ]);
   });
 
   test('getPlayerBySocket() gets players own info', async () => {
-    const res=await getPlayerBySocket(mockSocket);
+    const res = await getPlayerBySocket(mockSocket);
 
-    expect(db.Player.findOne).toBeCalledWith({socket: mockSocket},expect.any(Function));
+    expect(db.Player.findOne).toBeCalledWith(
+      {socket: mockSocket},
+      expect.any(Function),
+    );
     expect(res).toEqual(mockPlayer);
   });
 
   test('removePlayer() deletes player', async () => {
     await removePlayer(mockSocket);
 
-    expect(db.Player.deleteOne).toBeCalledWith({socket: mockSocket},expect.any(Function));
+    expect(db.Player.deleteOne).toBeCalledWith(
+      {socket: mockSocket},
+      expect.any(Function),
+    );
   });
 });
 
-afterAll(() => {
-
-});
+afterAll(() => {});
--- server/controllers/__tests__/room.js
+++ server/controllers/__tests__/room.js
@@ -1,11 +1,20 @@
 const {db} = require('../../models');
-const {getAvailableRoom, updateRoom, checkRoomStatus, updateStartGameStatus } = require('../room');
+const {
+  getAvailableRoom,
+  updateRoom,
+  checkRoomStatus,
+  updateStartGameStatus,
+} = require('../room');
 
 jest.mock('../../models');
 
 db.Room.aggregate.mockResolvedValue([{code: 'ASS', active: true}]);
 db.Room.updateOne.mockResolvedValue();
-db.Room.findOne.mockResolvedValue({active: true,gameStarted: false,code: 'ASS'});
+db.Room.findOne.mockResolvedValue({
+  active: true,
+  gameStarted: false,
+  code: 'ASS',
+});
 
 describe('getAvailableRoom()', () => {
   test('should get an inactive room', async () => {
@@ -37,18 +46,16 @@ describe('updateRoom()', () => {
 
 describe('checkRoomStatus()', () => {
   test('should return the data for the room', async () => {
-    const res=await checkRoomStatus('ASS');
+    const res = await checkRoomStatus('ASS');
 
-    expect(db.Room.findOne).toBeCalledWith(
-      {code: 'ASS'},
-    );
-    expect(res).toEqual({active: true,gameStarted: false,code: 'ASS'});
+    expect(db.Room.findOne).toBeCalledWith({code: 'ASS'});
+    expect(res).toEqual({active: true, gameStarted: false, code: 'ASS'});
   });
 });
 
 describe('updateStartGameStatus()', () => {
   test('should set the start status of the game to true', async () => {
-    const res=await updateStartGameStatus('ASS');
+    const res = await updateStartGameStatus('ASS');
 
     expect(db.Room.updateOne).toBeCalledWith(
       {code: 'ASS'},
