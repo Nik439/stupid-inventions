c1b48f4bab50d325ebc3b135f80f1aaa87d5ca62
P.Ackroyd <peter.r.ackroyd@gmail.com>
2020-11-25 19:46:22 +0000

    test(client/src/components/stages/drawing): jest is so wonky
    managed to test drawing after finding the least logical way to mock useref hook, couldnt figure out
    how to mock canvas so gave up and mocked entire component

---
+++ client/src/components/stages/Drawing/drawing.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import Drawing from '.';
+import MyComponent from '../../canvas/index.js';
+
+const MockMyComponent = () => {
+  return (<div></div>);
+};
+jest.mock("../../canvas/index.js", () => ({
+  __esModule: true,
+  namedExport: jest.fn(),
+  default: jest.fn()
+}));
+
+beforeAll(() => {
+  MyComponent.mockImplementation(MockMyComponent);
+});
+jest.mock('react', () => {
+  const originReact = jest.requireActual('react');
+  const mUseRef = jest.fn();
+  return {
+    ...originReact,
+    useRef: mUseRef,
+  };
+});
+jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: { toDataURL:()=>{return 5}}});
+//none of this makes sense but its the only way to get this to work and jest is really bad so dont touch it
+
+const submitInvention=jest.fn();
+describe('Drawing', () => {
+  test('pressing submit calls submit only when a invention name has been entered', () => {
+    jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: { toDataURL:()=>{return 5}}});
+
+    render(<Drawing submitInvention={submitInvention} />);
+
+    expect(submitInvention).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByRole('button'));
+    expect(submitInvention).not.toHaveBeenCalled();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {value: 'ASS'},
+    });
+    expect(screen.getByRole('textbox')).toHaveValue('ASS')
+    fireEvent.click(screen.getByRole('button'));
+    expect(submitInvention).toHaveBeenCalled();
+  });
+});
